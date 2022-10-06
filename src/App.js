1ffe16810d4bcfe521bf2a6af60565dd9eb0d9e4
Sanyi81 <skyharts2002@gmail.com>
2022-10-06 22:20:00 +0300

    commit cats

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
-import {Cat, Dog} from "./components";
+import {useReducer, useRef} from "react";
 
+const reducer = (state, action) => {
+    const {type, payload} = action;
 
-function App() {
+    switch (type) {
+        case 'addCat':
+            return {...state, cats: [...state.cats, {name: payload, id: Date.now ()}]};
+        case 'deleteCat':
+            return {...state, cats: state.cats.filter(cat => cat.id !== payload)};
+
+        case 'addDog':
+            return {...state, dogs: [...state.dogs, {name: payload, id: Date.now ()}]};
+        case 'deleteDog':
+            return {...state, dogs: state.dogs.filter(dog => dog.id !== payload)};
+
+        default:
+            console.error('');
+            return state;
+    }
+};
+
+    function App() {
+        const [state, dispatch] = useReducer(reducer, {cats:[], dogs:[]});
+
+        const catInput = useRef ();
+
+        const dogInput = useRef ();
+
+        const createCat = () => {
+            dispatch ({type:'addCat', payload: catInput.current.value});
+            catInput.current.value = '';
+        }
+
+        const createDog = () => {
+            dispatch ({type:'addDog', payload: dogInput.current.value});
+            dogInput.current.value = '';
+        }
+
   return (
       <div>
-            <Cat/>
-            <Dog/>
+          <div className={'cats'}>
+              <label>Cats name: <input type="text" ref={catInput}/></label>
+              <button onClick={createCat}>Save</button>
+          </div>
+
+          {
+              state.cats.map(cat => (<div key={cat.id}>
+              {cat.name}
+              <button onClick={() => dispatch({type: 'deleteCat', payload: cat.id})}>Delete</button>
+          </div>))
+          }
+
+          <div className={'dogs'}>
+              <label>Dogs name: <input type="text" ref={dogInput}/></label>
+              <button onClick={createDog}>Save</button>
+          </div>
+
+          {
+              state.dogs.map(dog => (<div key={dog.id}>
+                  {dog.name}
+                  <button onClick={() => dispatch({type: 'deleteDog', payload: dog.id})}>Delete</button>
+              </div>))
+          }
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Cat/Cat.js
+++ src/components/Cat/Cat.js
@@ -1,14 +1,1 @@
-import {useCatReducer} from "../../reducers";
-import {CatForm} from "./CatForm";
 
-const Cat = () => {
-    const [state, dispatch] = useCatReducer();
-        return (
-    <div>
-        <h2>Cat: {CatForm}</h2>
-        <button>Save</button>
-    </div>
-)
-}
-
-export {Cat};
